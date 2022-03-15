abd20b2b2bed6ac255e3bfffa1d7ca05bcb95c10
Will Beasley <will.beasley@outlook.com>
2022-03-15 22:55:01 +0000

    Modified to work with 10 channels and added pin definitions for each

---
+++ ControllerCode/src/defs.hpp
@@ -0,0 +1,52 @@
+#ifndef __defs_H__
+#define __defs_H__
+
+// FastLED I2S support enable
+#define FASTLED_ESP32_I2S
+
+// Array Dimensions
+#define X_NUM 38
+#define Y_NUM 28
+
+// Pin usage definitions
+#define CH1_PIN 23
+#define CH2_PIN 22
+#define CH3_PIN 21
+#define CH4_PIN 3
+#define CH5_PIN 21
+#define CH6_PIN 19
+#define CH7_PIN 18
+#define CH8_PIN 5
+#define CH9_PIN 17
+#define CH10_PIN 16
+
+// Channel LED Count ( Basically they all have 3 strips of 38 attached except for channel 9 and 10 with only 2 strips)
+#define CH1_NUM (3 * X_NUM)
+#define CH2_NUM (3 * X_NUM)
+#define CH3_NUM (3 * X_NUM)
+#define CH4_NUM (3 * X_NUM)
+#define CH5_NUM (3 * X_NUM)
+#define CH6_NUM (3 * X_NUM)
+#define CH7_NUM (3 * X_NUM)
+#define CH8_NUM (3 * X_NUM)
+#define CH9_NUM (2 * X_NUM)
+#define CH10_NUM (2 * X_NUM)
+
+// Offsets from the base for each channel
+#define CH1_OFFSET (0u)
+#define CH2_OFFSET (CH1_NUM)
+#define CH3_OFFSET (CH2_OFFSET + CH2_NUM)
+#define CH4_OFFSET (CH3_OFFSET + CH3_NUM)
+#define CH5_OFFSET (CH4_OFFSET + CH4_NUM)
+#define CH6_OFFSET (CH5_OFFSET + CH5_NUM)
+#define CH7_OFFSET (CH6_OFFSET + CH6_NUM)
+#define CH8_OFFSET (CH7_OFFSET + CH7_NUM)
+#define CH9_OFFSET (CH8_OFFSET + CH8_NUM)
+#define CH10_OFFSET (CH9_OFFSET + CH9_NUM)
+
+#define NUM_LEDS (X_NUM * Y_NUM)
+
+
+
+
+#endif
--- ControllerCode/src/main.cpp
+++ ControllerCode/src/main.cpp
@@ -1,39 +1,31 @@
 #include <Arduino.h>
-
-#define FASTLED_ESP32_I2S
+#include "defs.hpp"
 #include <FastLED.h>
 
-#define NUM_PINS 10
-const int LED_COUNT[NUM_PINS] = { 4, 4, 4, 4, 3, 3, 3, 3};
-const int PINS[NUM_PINS] = { 1, 2, 3, 4, 5, 6, 7, 8};
-#define X_DIM 38
-#define Y_DIM 28
-
-#define NUM_LEDS (X_DIM * Y_DIM)
-
-
+// One big block of CRGB values for all the leds
 CRGB leds[NUM_LEDS];
 
 void setup() {
-  // put your setup code here, to run once:  
-  int running_total = 0;
-  FastLED.addLeds<WS2812B,1>(leds, running_total, LED_COUNT[0]);
-  running_total += LED_COUNT[0];
-  FastLED.addLeds<WS2812B,2>(leds, running_total, LED_COUNT[1]);
-  running_total += LED_COUNT[1];
-  FastLED.addLeds<WS2812B,3>(leds, running_total, LED_COUNT[2]);
-  running_total += LED_COUNT[2];
-  FastLED.addLeds<WS2812B,4>(leds, running_total, LED_COUNT[3]);
-  running_total += LED_COUNT[3];
-  FastLED.addLeds<WS2812B,5>(leds, running_total, LED_COUNT[4]);
-  running_total += LED_COUNT[4];
-  FastLED.addLeds<WS2812B,13>(leds, running_total, LED_COUNT[5]);
-  running_total += LED_COUNT[5];
-  FastLED.addLeds<WS2812B,14>(leds, running_total, LED_COUNT[6]);
-  running_total += LED_COUNT[6];
-  FastLED.addLeds<WS2812B,15>(leds, running_total, LED_COUNT[7]);
-  running_total += LED_COUNT[7];
-  
+  // Setup code runs once at startup
+
+  // If we have USB UART then start it
+  //
+  Serial.begin(115200);
+
+  // Create the FastLed controllers, one per pin
+  //
+  FastLED.addLeds<WS2812B,CH1_PIN>(leds, CH1_OFFSET, CH1_NUM);
+  FastLED.addLeds<WS2812B,CH2_PIN>(leds, CH2_OFFSET, CH2_NUM);
+  FastLED.addLeds<WS2812B,CH3_PIN>(leds, CH3_OFFSET, CH3_NUM);
+  FastLED.addLeds<WS2812B,CH4_PIN>(leds, CH4_OFFSET, CH4_NUM);
+  FastLED.addLeds<WS2812B,CH5_PIN>(leds, CH5_OFFSET, CH5_NUM);
+  FastLED.addLeds<WS2812B,CH6_PIN>(leds, CH6_OFFSET, CH6_NUM);
+  FastLED.addLeds<WS2812B,CH7_PIN>(leds, CH7_OFFSET, CH7_NUM);
+  FastLED.addLeds<WS2812B,CH8_PIN>(leds, CH8_OFFSET, CH8_NUM);
+  FastLED.addLeds<WS2812B,CH9_PIN>(leds, CH9_OFFSET, CH9_NUM);
+  FastLED.addLeds<WS2812B,CH10_PIN>(leds, CH10_OFFSET, CH10_NUM);
+
+    
 }
 
 void loop() {
